5eb23407ea8213e44266fab61a8a1290af124f37
Paul Debus <debuspaul@gmail.com>
2023-06-29 00:00:40 +0200

    fix test script, does something now, but not what I want

--- test.jl
+++ test.jl
@@ -2,13 +2,21 @@ using StaticArrays
 using Makie
 using GLMakie
 using ImageProjectiveGeometry
-
+GLMakie.activate!()
+Makie.inline!(false)
 using AdaptivePotentialField
+using GeometryBasics
 
 vp = PolarViewpoint(SVector(0., 0, 0), 0., 0.)
+qp = QuatViewpoint(vp)
 scene = Scene()
 cam3d!(scene)
+arrows!(scene, [Point3(0, 1., 0)], [Vec3(1., 0, 0)], color=:red)
+scene
 
-cam = convert(ImageProjectiveGeometry.Camera, vp)
+cam = convert(ImageProjectiveGeometry.Camera, qp)
+cam.rows = 100
+cam.cols = 100
 
-AdaptivePotentialField.plotcamera(cam, 4., scene)
+subscene = AdaptivePotentialField.plotcamera(cam, 4., scene);
+center!(scene)
